6a4ca06e870c2dc09503ae41d2587eca1f6da98d
oolgei <110331130+oolgei@users.noreply.github.com>
2022-08-10 03:53:46 +0000

    Update gost-heroku.bat

--- gost-heroku.bat
+++ gost-heroku.bat
@@ -1,4 +1,4 @@
 @echo off
 color 0a
-title gost-windows-2.11.0
-gost.exe -L=:1080 -F=ss+mwss://method:passowrd@appname.herokuapp.com:443
+title gost-windows-2.12.0
+gost.exe -L=:1080 -F=ss+mwss://method:passowrd@服务器.com:443
